0e4a9bd9f7e39f3544ce09df4eff15658bb820d9
Angel-smith974 <elfedelanuit974@hotmail.fr>
2019-09-30 12:36:59 +0400

    premier commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Les boucles PHP</title>
+</head>
+<body>
+    <h1>Les boucles en PHP</h1>
+
+    <?php
+    /*
+    for($nombre=0;$nombre<=10;$nombre++){
+        echo "Le nombre est de $nombre <br>";
+    }
+    */
+
+    $var01 = 0;
+    $var02 = 10;
+
+    for($var01;$var01<=20;$var01++){
+        echo $var01*$var02 . "<br>";
+    }
+
+
+
+
+
+    ?>
+
+
+
+
+
+</body>
+</html>
